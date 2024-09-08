e9de192b41aba3cf56c7369a3d1e994b6be23071
Amanpati Tripathi <tripathiaman777@gmail.com>
2024-09-08 16:56:54 +0530

    Unwanted dependency removed

--- frontend/src/pages/Billings.jsx
+++ frontend/src/pages/Billings.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react";
 import { useBillStore } from "../store/billStore";
-import PDFViewer from "../components/worker";
 import { useNavigate } from "react-router-dom";
 import EmptyState from "../components/EmptyPage";
 
